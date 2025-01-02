8021c8f5f1ea2601947599d08915289192172a4d
Pawel Kluska <pkluska@griddynamics.com>
2025-01-02 22:16:13 +0100

    init project

---
+++ src/test/java/com/shopassistant/ShopAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
